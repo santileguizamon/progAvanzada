0590a80446040c9041465f115bce1afdf6781168
santileguizamon <103467628+santileguizamon@users.noreply.github.com>
2024-04-30 14:18:21 -0300

    aclaraciones

--- src/Main.java
+++ src/Main.java
@@ -12,11 +12,11 @@ public class Main {
 			opcion = JOptionPane.showOptionDialog(null, "Seleccione la opcion a realizar", null, 0, 0, null, menu, menu[0]);
 			switch (opcion) {
 			case 0: 
-				//crearReservaVuelo();
+				//crearReservaVuelo(); su funcion seria buscar un vuelo, seleccionarlo, y crear la reserva del vuelo en donde esta devuelve un ticket con la info necesaria
 				break;
 			case 1:
-				//crearReservaVuelo();
-				//crearReservaDestino();
+				//crearReservaVuelo(); lo mismo que la de arriba 
+				//crearReservaDestino(); busca un hotel en el destino seleccionado y crea una reserva con su ticket
 				break;
 				
 			}
